b721dbb1051264e8a50a2005a45b61e50934802e
Sanjeever <78210374+Sanjeever@users.noreply.github.com>
2022-12-15 19:01:15 +0800

    🚨 lang="ts"

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,7 +5,7 @@
   </div>
 </template>
 
-<script setup>
+<script setup lang="ts">
 import { aplayer } from "@/config/aplayer";
 import { sound } from "@/config/howler";
 import isMobile from "is-mobile";
