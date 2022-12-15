3de9e2448e0d5e43e3ef3ce45675e9ec93a3eee4
Sanjeever <78210374+Sanjeever@users.noreply.github.com>
2022-12-15 17:42:13 +0800

    ✨ feat: import APlayer to accomplish a PlayList (WIP)

---
+++ src/config/aplayer-options-audio.ts
@@ -0,0 +1,36 @@
+import type { VNode } from "vue";
+type AudioType = "auto" | "hls" | "normal";
+interface Audio {
+  [index: number]: this;
+
+  id?: number;
+  name: string | VNode; // eslint-disable-line no-restricted-globals
+  artist: string | VNode;
+  url: string;
+  cover?: string;
+  lrc?: string;
+  theme?: string;
+  type?: AudioType;
+  speed?: number;
+}
+
+export const audioList: Audio[] = [
+  {
+    name: "Mambo Santa Mambo",
+    artist: "The Enchanters",
+    url: "https://files-1251985639.cos.ap-shanghai.myqcloud.com/xmas/music/Mambo%20Santa%20Mambo.mp3",
+    cover: "https://pic.imgdb.cn/item/639add6eb1fccdcd365b276b.jpg",
+  },
+  {
+    name: "We Wish You a Merry Christmas",
+    artist: "Christmas Carol & Song",
+    url: "https://files-1251985639.cos.ap-shanghai.myqcloud.com/xmas/music/We%20Wish%20You%20a%20Merry%20Christmas%20with%20Lyrics%20_%20Christmas%20Carol%20%26%20Song.mp3",
+    cover: "https://pic.imgdb.cn/item/639ae2a5b1fccdcd3662f190.png",
+  },
+  {
+    name: "Have Yourself a Merry Little Christmas (feat. j'san)",
+    artist: "Rook1e / J'san",
+    url: "https://files-1251985639.cos.ap-shanghai.myqcloud.com/xmas/music/Rook1e%2CJ%27san%20-%20Have%20Yourself%20a%20Merry%20Little%20Christmas%20%28feat.%20j%27san%29.mp3",
+    cover: "https://pic.imgdb.cn/item/639ae0a0b1fccdcd36602363.jpg",
+  },
+];
