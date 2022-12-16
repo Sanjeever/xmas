c899ba32ed784d81fcdeb1850162ccad0d556c1c
Sanjeever <78210374+Sanjeever@users.noreply.github.com>
2022-12-16 21:27:22 +0800

    ➖ remove deps 'pinia'

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
 import { createApp } from "vue";
-import { createPinia } from "pinia";
 
 import App from "./App.vue";
 import router from "./router";
@@ -9,7 +8,6 @@ import "aplayer/dist/APlayer.min.css";
 
 const app = createApp(App);
 
-app.use(createPinia());
 app.use(router);
 
 app.mount("#app");
