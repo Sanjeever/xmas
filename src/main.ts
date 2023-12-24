54b854cf9c53ce106a5a9dc081af5fc71a8b83dd
Sanjeever <78210374+Sanjeever@users.noreply.github.com>
2023-12-24 21:36:42 +0800

    🐞 fix: build fail

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import App from './App'
 import 'virtual:uno.css'
 import '@unocss/reset/tailwind-compat.css'
-import 'APlayer/dist/APlayer.min.css';
+import "aplayer/dist/APlayer.min.css";
 import './assets/main.css'
 import './assets/dot-loader.css'
 
